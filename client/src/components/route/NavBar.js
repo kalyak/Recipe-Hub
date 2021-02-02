d34b9c115edbee98ee6326a4db7eb3e6acbc3300
sophiaratna <sophia.ratna92@gmail.com>
2021-02-02 23:52:44 +0800

    set up tentative routing

---
+++ client/src/components/route/NavBar.js
@@ -0,0 +1,62 @@
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand as={Link} to="/home">
+        RECIPE HUB
+      </Navbar.Brand>
+      <Nav className="mr-auto" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link as={Link} to="/browse">
+            Browse Recipe
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/random">
+            Random Recipe
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/search">
+            Search Bar
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* check if not logged in yet */}
+      <Nav>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/login">
+            Login
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/signup">
+            Sign Up
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* if already login */}
+      <Nav>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/fave">
+            Faved
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/planner">
+            Planner
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/logout">
+            Logout
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
+  );
+};
+
+export default NavBar;
