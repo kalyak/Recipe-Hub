2304074e1ba7fce808c0bd9637d3dd39f4127e9f
aukokyong <aukokyong@gmail.com>
2021-02-03 12:27:32 +0800

    page template for login and signup page

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
+
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [isLogin, setIsLogin] = useState(false);
+  const [resErrMsg, setResErrMsg] = useState({ username: "", password: "" });
+
+  const handleSubmit = (event) => {
+    console.log(formData);
+    event.preventDefault();
+    axios
+      .post("/", formData)
+      .then((response) => {
+        console.log(response);
+        setIsLogin(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    // console.log(event.target.value);
+    setFormData((state) => {
+      return { ...state, [event.target.name]: event.target.value };
+    });
+    setResErrMsg({ username: "", password: "" });
+  };
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" onChange={(e) => handleChange(e)} />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="text" name="password" onChange={(e) => handleChange(e)} />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 };
 
 export default LoginPage;
--- client/src/components/pages/SignupPage.js
+++ client/src/components/pages/SignupPage.js
@@ -1,5 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
 const SignupPage = () => {
-  return <h1>Signup Page</h1>;
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [resErrMsg, setResErrMsg] = useState({ username: "", password: "" });
+
+  const handleSubmit = (event) => {
+    console.log(formData);
+    event.preventDefault();
+    axios
+      .post("/", formData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    setFormData((state) => {
+      return { ...state, [event.target.name]: event.target.value };
+    });
+    setResErrMsg({ username: "", password: "" });
+  };
+
+  return (
+    <>
+      <h1>Signup Page</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="username">Username:</label>
+        <input type="text" name="username" onChange={(e) => handleChange(e)} />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="text" name="password" onChange={(e) => handleChange(e)} />
+        <br />
+        <br />
+        <button type="submit">Create Account</button>
+      </form>
+    </>
+  );
 };
 
 export default SignupPage;
