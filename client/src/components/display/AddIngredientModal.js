62db3203ae844f233586aa56d10767ad01828bff
sophiaratna <sophia.ratna92@gmail.com>
2021-02-08 23:46:53 +0800

    add dropdown for ingredient type & add dropdown for measurement unit

--- client/src/components/display/AddIngredientModal.js
+++ client/src/components/display/AddIngredientModal.js
@@ -4,7 +4,13 @@ import { Modal, Button } from "react-bootstrap";
 
 const AddIngredientModal = (props) => {
   //   const [openIngredientModal, setOpenIngredientModal] = useState(false);
-  const [formData, setFormData] = useState({ ingredientName: "", units: [""] });
+  const initialState = {
+    ingredientName: "",
+    units: [""],
+    type: "",
+  };
+  const [formData, setFormData] = useState(initialState);
+  const measurementUnits = ["", "tsp", "tbsp", "g", "ml", "clove", "packs"];
 
   const handleChange = (event) => {
     setFormData((state) => {
@@ -13,8 +19,10 @@ const AddIngredientModal = (props) => {
   };
 
   const handleChangeUnit = (event, index) => {
+    console.log(event.target.value);
     const selectedUnits = [...formData.units];
     selectedUnits[index] = event.target.value;
+    console.log("units", selectedUnits);
     setFormData((state) => {
       return { ...state, units: selectedUnits };
     });
@@ -33,6 +41,11 @@ const AddIngredientModal = (props) => {
     setFormData((state) => {
       return { ...state, units: selectedUnits };
     });
+  };
+
+  const handleCancel = () => {
+    setFormData(initialState);
+    props.onHide();
   };
 
   const handleSubmit = (event) => {
@@ -64,7 +77,7 @@ const AddIngredientModal = (props) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Pls key in the ingredient name and unit measurement</h4>
+          <h4>Please fill up all the empty fields</h4>
           <form onSubmit={handleSubmit}>
             <label htmlFor="ingredientName">Ingredient Name:</label> <br />
             <input
@@ -81,6 +94,42 @@ const AddIngredientModal = (props) => {
             {formData.units.map((unit, index) => {
               return (
                 <>
+                  <select
+                    id="units"
+                    name="units"
+                    value={formData.units[index]}
+                    onChange={(event) => handleChangeUnit(event, index)}
+                    required={true}
+                  >
+                    {measurementUnits.map((unit) => {
+                      return <option value={unit}>{unit}</option>;
+                    })}
+                  </select>
+                  {index === formData.units.length - 1 && (
+                    <Button onClick={() => addUnits()}>+</Button>
+                  )}
+                  {index !== formData.units.length - 1 && (
+                    <Button onClick={() => removeUnits(index)}>-</Button>
+                  )}
+                  <br />
+                </>
+              );
+            })}
+            <br />
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Button type="submit">Add</Button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default AddIngredientModal;
+
+{
+  /* <>
                   <input
                     type="text"
                     id="units"
@@ -96,18 +145,5 @@ const AddIngredientModal = (props) => {
                     <Button onClick={() => removeUnits(index)}>-</Button>
                   )}
                   <br />
-                </>
-              );
-            })}
-            <br />
-            <Button onClick={props.onHide}>Cancel</Button>
-            <Button type="submit">Add</Button>
-          </form>
-        </Modal.Body>
-        <Modal.Footer></Modal.Footer>
-      </Modal>
-    </>
-  );
-};
-
-export default AddIngredientModal;
+                </> */
+}
