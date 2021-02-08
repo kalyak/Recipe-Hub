b577afaaacb5898cfba3b61e84e9243fd6d11a00
aukokyong <aukokyong@gmail.com>
2021-02-08 16:21:39 +0800

    resolved api call

--- client/src/components/pages/BrowseRecipePage.js
+++ client/src/components/pages/BrowseRecipePage.js
@@ -3,7 +3,7 @@ import { Row, Col, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import HomePageData from "./sampleData";
-import QueryResultsDisplay from "../display/QueryResultsDisplay";
+import BrowseResultsDisplay from "../display/QueryResultsDisplay";
 import sampleimg from "./sampleimage.jpg";
 
 const BrowseRecipePage = () => {
@@ -95,51 +95,51 @@ const BrowseRecipePage = () => {
   ]);
   const [browsingTag, setBrowsingTag] = useState({ tag: "" });
   const [queryResults, setQueryResults] = useState([
-    {
-      _id: "1",
-      tags: ["lunch", "dinner"],
-      recipeName: "Egg Fried Rice1",
-      description:
-        "1If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
-      avgRating: 3.5,
-      imageURL: sampleimg,
-    },
-    {
-      _id: "2",
-      tags: ["breakfast", "lunch"],
-      recipeName: "Egg Fried Rice2",
-      description:
-        "2If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
-      avgRating: 3.5,
-      imageURL: sampleimg,
-    },
-    {
-      _id: "3",
-      tags: ["breakfast", "supper"],
-      recipeName: "Egg Fried Rice3",
-      description:
-        "3If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
-      avgRating: 3.5,
-      imageURL: sampleimg,
-    },
-    {
-      _id: "4",
-      tags: ["breakfast", "main"],
-      recipeName: "Egg Fried Rice4",
-      description:
-        "4If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
-      avgRating: 3.5,
-      imageURL: sampleimg,
-    },
-    {
-      _id: "5",
-      tags: ["side dish", "chinese"],
-      recipeName: "Egg Fried Rice5",
-      description:
-        "5If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
-      avgRating: 3.5,
-      imageURL: sampleimg,
-    },
+    // {
+    //   _id: "1",
+    //   tags: ["lunch", "dinner"],
+    //   recipeName: "Egg Fried Rice1",
+    //   description:
+    //     "1If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
+    //   avgRating: 3.5,
+    //   imageURL: sampleimg,
+    // },
+    // {
+    //   _id: "2",
+    //   tags: ["breakfast", "lunch"],
+    //   recipeName: "Egg Fried Rice2",
+    //   description:
+    //     "2If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
+    //   avgRating: 3.5,
+    //   imageURL: sampleimg,
+    // },
+    // {
+    //   _id: "3",
+    //   tags: ["breakfast", "supper"],
+    //   recipeName: "Egg Fried Rice3",
+    //   description:
+    //     "3If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
+    //   avgRating: 3.5,
+    //   imageURL: sampleimg,
+    // },
+    // {
+    //   _id: "4",
+    //   tags: ["breakfast", "main"],
+    //   recipeName: "Egg Fried Rice4",
+    //   description:
+    //     "4If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
+    //   avgRating: 3.5,
+    //   imageURL: sampleimg,
+    // },
+    // {
+    //   _id: "5",
+    //   tags: ["side dish", "chinese"],
+    //   recipeName: "Egg Fried Rice5",
+    //   description:
+    //     "5If you are busy with all the preparations for the festive season, this oldie but goodie is just the thing to whip up for a simple one-dish homecooked meal.",
+    //   avgRating: 3.5,
+    //   imageURL: sampleimg,
+    // },
   ]);
 
   useEffect(() => {
@@ -161,7 +161,7 @@ const BrowseRecipePage = () => {
       .get(url)
       .then((response) => {
         console.log(response.data);
-        // setQueryResults(response.data);
+        setQueryResults([...response.data]);
       })
       .catch((error) => {
         console.log(error);
@@ -225,7 +225,7 @@ const BrowseRecipePage = () => {
       <h1>Browse by categories for your meal</h1>
       <Row>{displayTags}</Row>
       {browsingTag.tag !== "" ? (
-        <QueryResultsDisplay filteredResults={queryResults} />
+        <BrowseResultsDisplay filteredResults={queryResults} />
       ) : (
         ""
       )}
