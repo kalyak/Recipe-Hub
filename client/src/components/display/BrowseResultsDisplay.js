d0a4a66a73c2c2e82c7b96ee438a93ed3124b50e
aukokyong <aukokyong@gmail.com>
2021-02-10 11:47:19 +0800

    formatted browse page

--- client/src/components/display/BrowseResultsDisplay.js
+++ client/src/components/display/BrowseResultsDisplay.js
@@ -23,31 +23,31 @@ const BrowseResultsDisplay = (props) => {
     const updatedDate = dayjs(recipe.updatedAt).format("DD/MMM/YYYY");
 
     return (
-      <Col className='pb-5'>
+      <Col className="pb-5" key={recipe._id}>
         <Card
           key={recipe._id}
           style={{ width: "18rem" }}
           // className='mb-5 ml-5'
-          className='h-100'
+          className="h-100"
         >
-          <Card.Img width={288} height={216} variant='top' src={image} />
+          <Card.Img width={288} height={216} variant="top" src={image} />
           <Card.Body>
-            <Card.Title className='text-capitalize'>
+            <Card.Title className="text-capitalize">
               {recipe.recipeName}
               <ReactStars value={recipe.avgRating} edit={false} isHalf={true} />
             </Card.Title>
             <Card.Text
               style={{
-                height: "5rem",
-                "overflow-y": "hidden",
-                "text-overflow": "ellipsis",
+                height: "6rem",
+                overflowY: "auto",
+                textOverflow: "ellipsis",
               }}
             >
               {recipe.description}
             </Card.Text>
-            <Row className='justify-content-md-center'>
+            <Row className="justify-content-md-center">
               <Link to={`/recipe/${recipe._id}`}>
-                <Button variant='primary'>Show More</Button>
+                <Button variant="primary">Show More</Button>
               </Link>
             </Row>
           </Card.Body>
@@ -57,8 +57,14 @@ const BrowseResultsDisplay = (props) => {
               .map((tag) => {
                 return (
                   <Fragment key={tag._id}>
-                    <Link to={`/browse?tag=${tag._id}`}>
-                      <Badge className='text-capitalize' variant='success'>
+                    <Link to={`/browse`}>
+                      <Badge
+                        className="text-capitalize"
+                        variant="success"
+                        onClick={() => {
+                          props.setBrowsingTag({ tag: tag._id });
+                        }}
+                      >
                         {tag.tagName}
                       </Badge>
                     </Link>
@@ -66,7 +72,7 @@ const BrowseResultsDisplay = (props) => {
                 );
               })}
             <br />
-            <small className='text-muted'>Updated on: {updatedDate}</small>
+            <small className="text-muted">Updated on: {updatedDate}</small>
           </Card.Footer>
         </Card>
       </Col>
@@ -75,14 +81,17 @@ const BrowseResultsDisplay = (props) => {
 
   return (
     <Container>
-      <h1>Results Display</h1>
+      <Row className="justify-content-md-center">
+        <h1>Results</h1>
+      </Row>
+      <br />
 
       {props.filteredResults.length > 0 ? (
-        <CardDeck className='row row-cols-1 row-cols-sm-2 row-cols-lg-3'>
+        <CardDeck className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
           {display}
         </CardDeck>
       ) : (
-        <h4 className='text-center'>No results found</h4>
+        <h4 className="text-center">No Results Found</h4>
       )}
     </Container>
   );
