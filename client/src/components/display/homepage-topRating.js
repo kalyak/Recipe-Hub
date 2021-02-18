52119f91ad2db01d3a68ea21af72128fab4d5ce3
kalyak <kalyakusumadi@gmail.com>
2021-02-18 21:12:55 +0800

    Changed display on homepage to carousel.

--- client/src/components/display/homepage-topRating.js
+++ client/src/components/display/homepage-topRating.js
@@ -13,6 +13,7 @@ import axios from "axios";
 import ReactStars from "react-rating-stars-component";
 import noImage from "../icons/600px-No_image_available_600_x_450.png";
 import dayjs from "dayjs";
+import RecipeCarousel from "./Carousel";
 
 const TopRating = () => {
   const [topRating, setTopRating] = useState([]);
@@ -31,78 +32,7 @@ const TopRating = () => {
 
   return (
     <Container>
-      <CardDeck className="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
-        {topRating.length > 0 &&
-          topRating.map((recipe) => {
-            const image = recipe.imgURL ? recipe.imgURL : noImage;
-            const updatedDate = dayjs(recipe.createdAt).format("DD MMM YYYY");
-
-            return (
-              <Col className="pb-5" key={recipe._id}>
-                <Card
-                  key={recipe._id}
-                  style={{ width: "18rem" }}
-                  className="h-100"
-                >
-                  <Card.Img
-                    width={288}
-                    height={216}
-                    variant="top"
-                    src={image}
-                  />
-                  <Card.Body>
-                    <Card.Title className="text-capitalize">
-                      {recipe.recipeName}
-                      <ReactStars
-                        value={recipe.avgRating}
-                        edit={false}
-                        isHalf={true}
-                      />
-                    </Card.Title>
-                    <Card.Text
-                      style={{
-                        height: "6rem",
-                        overflowY: "auto",
-                        textOverflow: "ellipsis",
-                      }}
-                    >
-                      {recipe.description}
-                    </Card.Text>
-                    <Row className="justify-content-md-center">
-                      <Link to={`/recipe/${recipe._id}`}>
-                        <Button variant="primary">Show More</Button>
-                      </Link>
-                    </Row>
-                  </Card.Body>
-                  <Card.Footer>
-                    {recipe.tags
-                      .sort((a, b) => (b.tagName > a.tagName ? 1 : -1))
-                      .reverse()
-                      .map((tag) => {
-                        // console.log(tag);
-                        return (
-                          <Fragment key={tag._id}>
-                            <Link to={`/browse?tag=${tag._id}`}>
-                              <Badge
-                                className="text-capitalize"
-                                variant="success"
-                              >
-                                {tag.tagName}
-                              </Badge>
-                            </Link>
-                          </Fragment>
-                        );
-                      })}
-                    <br />
-                    <small className="text-muted">
-                      Updated on: {updatedDate}
-                    </small>
-                  </Card.Footer>
-                </Card>
-              </Col>
-            );
-          })}
-      </CardDeck>
+      <RecipeCarousel recipes={topRating} />
     </Container>
   );
 };
